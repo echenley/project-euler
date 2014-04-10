74bb093749d6f4e2b264facc24c9d08c3a37f623
echenley <evan.henley@gmail.com>
2014-04-10 01:34:53 -0500

    more fancy 48

--- problem-048-self-powers.rb
+++ problem-048-self-powers.rb
@@ -1,9 +1,5 @@
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
-sum = 0
-
-for x in (1..1000)
-  sum += x ** x
-end
+sum = (1..1000).inject(0) { |sum,i| sum + i ** i }
 
 p sum.to_s[-10..-1].to_i
