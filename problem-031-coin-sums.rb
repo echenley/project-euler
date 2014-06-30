2e18fba69ba4d43df97965e5e1f5c70b92ac512c
Evan Henley <evan.henley@gmail.com>
2014-06-30 17:13:14 -0500

    Update problem-031-coin-sums.rb

--- problem-031-coin-sums.rb
+++ problem-031-coin-sums.rb
@@ -11,17 +11,17 @@ How many different ways can £2 (200p) be made using any number of coins?
 COINS = [200,100,50,20,10,5,2,1]
 combos = 0
 
-def add_coin(type,subtotal)          # recursive loop from higher value coins to lower
+def add_coin(type,subtotal)           # recursive loop from higher value coins to lower
   sub_combos = 0
   subtotal += COINS[type]             # adds current coin to subtotal
   return 1 if subtotal == COINS[0]    # returns 1 to combos if 200 is reached
-  for x in type...COINS.length     # otherwise adds another coin, and so on...
+  for x in type...COINS.length        # otherwise adds another coin, and so on...
     sub_combos += add_coin(x,subtotal) if subtotal + COINS[x] <= COINS[0]
   end
   return sub_combos                   # returns accumulated sub_combos
 end
 
-for type in 0...COINS.length       # picks starting coin
+for type in 0...COINS.length          # picks starting coin
   combos += add_coin(type,0)
   p [type,combos]
 end
