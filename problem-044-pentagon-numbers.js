3a4a9ac58fb322bf98e7086b46a7dcdbd6cc810b
echenley <evan.henley@gmail.com>
2015-02-21 19:32:32 -0600

    slight modifications

--- problem-044-pentagon-numbers.js
+++ problem-044-pentagon-numbers.js
@@ -2,53 +2,39 @@
 
 let pentagonNums = [];
 
-var memoize = function(func) {
-    var stringifyJson = JSON.stringify,
-        cache = {};
-
-    var cachedfun = function() {
-        var hash = stringifyJson(arguments);
-        return (hash in cache) ? cache[hash] : cache[hash] = func.apply(this, arguments);
-    };
-
-    cachedfun.__cache = (function() {
-        cache.remove || (cache.remove = function() {
-            var hash = stringifyJson(arguments);
-            return (delete cache[hash]);
-        });
-        return cache;
-    }).call(this);
-
-    return cachedfun;
-};
-
 function compose(func1, func2) {
     return function() {
         return func1(func2.apply(null, arguments));
     };
 }
 
-let pentagonalFormula = memoize(function(n) {
+function pentagonalFormula(n) {
     return n * (3 * n - 1) / 2;
-});
+}
 
 function addPentagonNum(n) {
     return (pentagonNums[n] = pentagonalFormula(n));
 }
 
-let isPentagonal = memoize(function(n) {
+function isPentagonal(n) {
     let largestPentagonNum = pentagonNums[pentagonNums.length-1];
     while (largestPentagonNum < n) {
         largestPentagonNum = addPentagonNum(pentagonNums.length);
     }
     return pentagonNums.indexOf(n) !== -1;
-});
+}
+
+function isPentagonalFast(n) {
+   isPentagonalFast.memoize = isPentagonalFast.memoize || {};
+   return (n in isPentagonalFast.memoize) ?
+   isPentagonalFast.memoize[n] : isPentagonalFast.memoize[n] = isPentagonal(n);
+}
 
 function sum(a, b) { return a + b; }
 function difference(a, b) { return Math.abs(a - b); }
 
-let sumIsPentagonal = compose(isPentagonal, sum);
-let differenceIsPentagonal = compose(isPentagonal, difference);
+let sumIsPentagonal = compose(isPentagonalFast, sum);
+let differenceIsPentagonal = compose(isPentagonalFast, difference);
 
 function satisfiesProblem44(a, b) {
     return differenceIsPentagonal(a, b) && sumIsPentagonal(a, b);
